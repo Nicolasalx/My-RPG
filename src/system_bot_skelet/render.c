cc53059f2a0ca421ced9b2eaeb3a40a9615b2fc7
Nicolasalx <nicolas1.alexandre@epitech.eu>
2023-04-11 19:31:12 +0200

    [MOD] level skelet

--- src/system_bot_skelet/render.c
+++ src/system_bot_skelet/render.c
@@ -80,13 +80,13 @@ void check_if_player_enter(int i)
     sfVector2f circlePos = sfCircleShape_getPosition(system_bot[i].zone);
     sfVector2f rectSize = sfRectangleShape_getSize(player.collision);
     float circleRadius = system_bot[i].radius;
-    
+    circlePos.x += circleRadius;
+    circlePos.y += circleRadius;
     float distance = sqrt((circlePos.x - rectPos.x) * (circlePos.x -
         rectPos.x) +
         (circlePos.y - rectPos.y) * (circlePos.y - rectPos.y));
     float rectHalfWidth = rectSize.x / 2.f;
     float rectHalfHeight = rectSize.y / 2.f;
-
     if (distance > circleRadius + rectHalfWidth && distance >
         circleRadius + rectHalfHeight) {
         system_bot[i].bot_can_move = false;
