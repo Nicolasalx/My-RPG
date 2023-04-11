d369d1df69f265dfd8edaf2d1419524844bbf605
thibaudcathala <thibaud.cathala@epitech.eu>
2023-04-11 19:15:49 +0200

    [FIX] conflict

---
+++ include/manage_view.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** B-MUL-200-PAR-2-1-myrpg-thibaud.cathala
+** File description:
+** manage_view
+*/
+
+#ifndef MANAGE_VIEW_H_
+    #define MANAGE_VIEW_H_
+
+    #include <SFML/Graphics.h>
+
+extern sfView *window_view;
+
+#endif /* !MANAGE_VIEW_H_ */
