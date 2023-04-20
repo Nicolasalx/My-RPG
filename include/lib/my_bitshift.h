0a42e06748fd274e02337131ac777bf84525ff67
thibaudcathala <thibaud.cathala@epitech.eu>
2023-04-20 12:18:44 +0200

    [ADD]

--- include/lib/my_bitshift.h
+++ include/lib/my_bitshift.h
@@ -16,5 +16,6 @@ void set_all_bit_to_zero(unsigned char *c);
 void set_bit_to_one(unsigned char *c, int index_bit);
 void set_bit_to_zero(unsigned char *c, int index_bit);
 void *my_memset(void *data, int value, size_t size);
+void my_memcpy(void *dest, const void *src, size_t size);
 
 #endif /* !MY_BITSHIFT_H_ */
