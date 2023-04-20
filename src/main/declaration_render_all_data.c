cc99946dbf6beb0b3067c108e2bf857d559821d2
Beauvais Jules <jules.beauvais@epitech.eu>
2023-04-20 15:47:50 +0200

    [DEL] player render x2 caused by merge in declaration_all_data.c

--- src/main/declaration_render_all_data.c
+++ src/main/declaration_render_all_data.c
@@ -38,7 +38,6 @@ render_func_t render_func[] = {
     {render_xp_heal_bar, GAME},
     {render_game_menu_button, GAME},
     {detect_shop_open, GAME},
-    {render_player, GAME},
     {render_door, GAME},
     {render_menu_quest, GAME},
     {NULL, 0}
