53f8feb4d4c7e0100d9cf2136084e66c13f21339
Nicolasalx <nicolas1.alexandre@epitech.eu>
2023-04-23 19:17:54 +0200

    [ADD] Free touret arrow and heal xp bar

--- src/main/free_all_data.c
+++ src/main/free_all_data.c
@@ -7,12 +7,22 @@
 
 #include "main.h"
 #include "inventory.h"
+#include "level_arrow.h"
+#include "heal_xp_bar.h"
 
 void free_all_data(void)
 {
     for (int i = 0; i < size_clothes_inventory; ++i) {
         sfRectangleShape_destroy(clothes_inventory[i].rectangle);
         sfSprite_destroy(clothes_inventory[i].sprite);
+    }
+    for (int i = 0; i < size_touret_arrow; ++i) {
+        sfCircleShape_destroy(touret_arrow[i].arrow);
+        sfSprite_destroy(touret_arrow[i].sprite);
+    }
+    for (int i = 0; i < size_heal_xp_bar; ++i) {
+        sfRectangleShape_destroy(heal_xp_bar[i].rectangle);
+        sfText_destroy(heal_xp_bar[i].text);
     }
     return;
 }
